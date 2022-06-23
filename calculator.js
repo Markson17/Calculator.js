4cf0ebc5a4fdc3a853604929f8614034c7c2c11d
Markson Favour <95889759+Markson17@users.noreply.github.com>
2022-06-23 06:18:20 +0100

    Initial commit

--- calculator.js
+++ calculator.js
@@ -1,27 +1,27 @@
-num1 = parseFloat(prompt("Enter the first number:"));
-num2 = parseFloat(prompt("Enter the second number:"));
-opr = prompt("Enter an operator (+, -, /, *): ");
+let num1 = parseFloat(prompt("Enter the first number:"));
+let num2 = parseFloat(prompt("Enter the second number:"));
+let opr = prompt("Enter an operator (+, -, /, *): ");
 
 
 if (opr == "+") {
-    result = num1 + num2;
+    let result = num1 + num2;
     alert("The sum of the given numbers is " + result)
 
 }
 else if (opr == "-") {
-    result = num1 - num2;
+    let result = num1 - num2;
     alert("The difference of the given numbers is " + result)
 
 }
 
 else if (opr == "*") {
-    result = num1 * num2;
+    let result = num1 * num2;
     alert("The product of the given number is " + result)
 
 }
 
 else if (opr == "/") {
-    result = num1 / num2;
+    let result = num1 / num2;
     alert("The division of the given number is " + result)
 
 }
